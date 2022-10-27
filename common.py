80a2ae1d07faba1f5de0e89038daa8371ddee9b1
Zachary Smith <zacharys0117@gmail.com>
2022-10-26 23:44:51 -0500

    Adding method of data collection and visualization supplied via GeoLifeClef

---
+++ common.py
@@ -0,0 +1,94 @@
+# Provided by GeoLifeClef
+# https://github.com/maximiliense/GLC/blob/master/data_loading/pytorch_dataset.py
+
+from __future__ import annotations
+from pathlib import Path
+from typing import Optional, Union, TYPE_CHECKING
+
+import numpy as np
+from PIL import Image
+import tifffile
+
+if TYPE_CHECKING:
+    import numpy.typing as npt
+
+    Patches = npt.NDArray
+
+
+def load_patch(
+    observation_id: Union[int, str],
+    patches_path: Union[str, Path],
+    *,
+    data: Union[str, list[str]] = "all",
+    landcover_mapping: Optional[npt.NDArray] = None,
+    return_arrays: bool = True,
+) -> list[Patches]:
+    """Loads the patch data associated to an observation id
+    Parameters
+    ----------
+    observation_id : integer / string
+        Identifier of the observation.
+    patches_path : string / pathlib.Path
+        Path to the folder containing all the patches.
+    data : string or list of string
+        Specifies what data to load, possible values: 'all', 'rgb', 'near_ir', 'landcover' or 'altitude'.
+    landcover_mapping : 1d array-like
+        Facultative mapping of landcover codes, useful to align France and US codes.
+    return_arrays : boolean
+        If True, returns all the patches as Numpy arrays (no PIL.Image returned).
+    Returns
+    -------
+    patches : list of size 4 containing 2d array-like objects
+        Returns a list containing all the patches in the following order: RGB, Near-IR, altitude and landcover.
+    """
+    observation_id = str(observation_id)
+
+    region_id = observation_id[0]
+    if region_id == "1":
+        region = "patches-fr"
+    elif region_id == "2":
+        region = "patches-us"
+    else:
+        raise ValueError(
+            "Incorrect 'observation_id' {}, can not extract region id from it".format(
+                observation_id
+            )
+        )
+
+    subfolder1 = observation_id[-2:]
+    subfolder2 = observation_id[-4:-2]
+
+    filename = Path(patches_path) / region / subfolder1 / subfolder2 / observation_id
+
+    patches = []
+
+    if data == "all":
+        data = ["rgb", "near_ir", "landcover", "altitude"]
+
+    if "rgb" in data:
+        rgb_filename = filename.with_name(filename.stem + "_rgb.jpg")
+        rgb_patch = Image.open(rgb_filename)
+        if return_arrays:
+            rgb_patch = np.asarray(rgb_patch)
+        patches.append(rgb_patch)
+
+    if "near_ir" in data:
+        near_ir_filename = filename.with_name(filename.stem + "_near_ir.jpg")
+        near_ir_patch = Image.open(near_ir_filename)
+        if return_arrays:
+            near_ir_patch = np.asarray(near_ir_patch)
+        patches.append(near_ir_patch)
+
+    if "altitude" in data:
+        altitude_filename = filename.with_name(filename.stem + "_altitude.tif")
+        altitude_patch = tifffile.imread(altitude_filename)
+        patches.append(altitude_patch)
+
+    if "landcover" in data:
+        landcover_filename = filename.with_name(filename.stem + "_landcover.tif")
+        landcover_patch = tifffile.imread(landcover_filename)
+        if landcover_mapping is not None:
+            landcover_patch = landcover_mapping[landcover_patch]
+        patches.append(landcover_patch)
+
+    return patches
---
+++ pytorch_dataset.py
@@ -0,0 +1,176 @@
+# Provided by GeoLifeClef
+# https://github.com/maximiliense/GLC/blob/master/data_loading/pytorch_dataset.py
+
+from __future__ import annotations
+from pathlib import Path
+from typing import Callable, Optional, Union, TYPE_CHECKING
+
+import numpy as np
+import pandas as pd
+
+from torch.utils.data import Dataset
+from common import load_patch
+
+if TYPE_CHECKING:
+    import numpy.typing as npt
+    from common import Patches
+    from environmental_raster import PatchExtractor
+
+    Targets = npt.NDArray[np.int64]
+
+
+class GeoLifeCLEF2022Dataset(Dataset):
+    """Pytorch dataset handler for GeoLifeCLEF 2022 dataset.
+    Parameters
+    ----------
+    root : string or pathlib.Path
+        Root directory of dataset.
+    subset : string, either "train", "val", "train+val" or "test"
+        Use the given subset ("train+val" is the complete training data).
+    region : string, either "both", "fr" or "us"
+        Load the observations of both France and US or only a single region.
+    patch_data : string or list of string
+        Specifies what type of patch data to load, possible values: 'all', 'rgb', 'near_ir', 'landcover' or 'altitude'.
+    use_rasters : boolean (optional)
+        If True, extracts patches from environmental rasters.
+    patch_extractor : PatchExtractor object (optional)
+        Patch extractor to use if rasters are used.
+    transform : callable (optional)
+        A function/transform that takes a list of arrays and returns a transformed version.
+    target_transform : callable (optional)
+        A function/transform that takes in the target and transforms it.
+    """
+
+    def __init__(
+            self,
+            root: Union[str, Path],
+            subset: str,
+            *,
+            region: str = "both",
+            patch_data: str = "all",
+            use_rasters: bool = True,
+            patch_extractor: Optional[PatchExtractor] = None,
+            transform: Optional[Callable] = None,
+            target_transform: Optional[Callable] = None,
+    ):
+        self.root = Path(root)
+        self.subset = subset
+        self.region = region
+        self.patch_data = patch_data
+        self.transform = transform
+        self.target_transform = target_transform
+
+        possible_subsets = ["train", "val", "train+val", "test"]
+        if subset not in possible_subsets:
+            raise ValueError(
+                "Possible values for 'subset' are: {} (given {})".format(
+                    possible_subsets, subset
+                )
+            )
+
+        possible_regions = ["both", "fr", "us"]
+        if region not in possible_regions:
+            raise ValueError(
+                "Possible values for 'region' are: {} (given {})".format(
+                    possible_regions, region
+                )
+            )
+
+        if subset == "test":
+            subset_file_suffix = "test"
+            self.training_data = False
+        else:
+            subset_file_suffix = "train"
+            self.training_data = True
+
+        df_fr = pd.read_csv(
+            self.root
+            / "observations"
+            / "observations_fr_{}.csv".format(subset_file_suffix),
+            sep=";",
+            index_col="observation_id",
+        )
+        df_us = pd.read_csv(
+            self.root
+            / "observations"
+            / "observations_us_{}.csv".format(subset_file_suffix),
+            sep=";",
+            index_col="observation_id",
+        )
+
+        if region == "both":
+            df = pd.concat((df_fr, df_us))
+        elif region == "fr":
+            df = df_fr
+        elif region == "us":
+            df = df_us
+
+        if self.training_data and subset != "train+val":
+            ind = df.index[df["subset"] == subset]
+            df = df.loc[ind]
+
+        self.observation_ids = df.index
+        self.coordinates = df[["latitude", "longitude"]].values
+
+        if self.training_data:
+            self.targets = df["species_id"].values
+        else:
+            self.targets = None
+
+        # FIXME: add back landcover one hot encoding?
+        # self.one_hot_size = 34
+        # self.one_hot = np.eye(self.one_hot_size)
+
+        self.patch_extractor = None
+        if use_rasters:
+            if patch_extractor is None:
+                patch_extractor = PatchExtractor(self.root / "rasters", size=256)
+                patch_extractor.add_all_rasters()
+
+            self.patch_extractor = patch_extractor
+
+    def __len__(self) -> int:
+        return len(self.observation_ids)
+
+    def __getitem__(
+            self,
+            index: int,
+    ) -> Union[
+        Union[Patches, list[Patches]], tuple[Union[Patches, list[Patches]], Targets]
+    ]:
+        latitude = self.coordinates[index][0]
+        longitude = self.coordinates[index][1]
+        observation_id = self.observation_ids[index]
+
+        patches: Union[Patches, list[Patches]] = load_patch(
+            observation_id, self.root, data=self.patch_data
+        )
+
+        # FIXME: add back landcover one hot encoding?
+        # lc = patches[3]
+        # lc_one_hot = np.zeros((self.one_hot_size,lc.shape[0], lc.shape[1]))
+        # row_index = np.arange(lc.shape[0]).reshape(lc.shape[0], 1)
+        # col_index = np.tile(np.arange(lc.shape[1]), (lc.shape[0], 1))
+        # lc_one_hot[lc, row_index, col_index] = 1
+
+        # Extracting patch from rasters
+        if self.patch_extractor is not None:
+            environmental_patches = self.patch_extractor[(latitude, longitude)]
+            patches = patches + [environmental_patches]
+
+        # Concatenate all patches into a single tensor
+        if len(patches) == 1:
+            patches = patches[0]
+
+        if self.transform:
+            patches = self.transform(patches)
+
+        if self.training_data:
+            target = self.targets[index]
+
+            if self.target_transform:
+                target = self.target_transform(target)
+
+            return patches, target
+        else:
+            return patches
